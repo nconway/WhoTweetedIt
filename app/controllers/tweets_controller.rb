736fea0b8e16730a484a11d3c7e01af0bd118727
nconway <nicole.conway@gmail.com>
2015-04-23 19:53:38 -0700

    have divs working and random pop

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,33 +5,32 @@ class TweetsController < ApplicationController
 
 	def show
 		
-		def rand_pair!
-      		k,v = rand_pair
-      		delete( k )
-      		return k,v
-    	end
-
-		client = current_user.twitter_client
-		@friends = client.friends
 		#@friends_array = client.friends.name.to_a
-
 		#binding.pry
 		#puts "aaaaaaaaaaaaaaaaaaaaaaaaa"
 
+		@win = 2
+
+		client = current_user.twitter_client
+		@friends = client.friends
+		
 		@name_list = Array.new
 		@tweet_list = Array.new
+		@hash_array = Array.new
 
 		@friends.attrs[:users].each do |todo|
   			@name_list.push(todo[:name])
   			@tweet_list.push(todo[:status][:text])
+  		end
 
   		#creates a hash from the two arrays
 		@name_tweet_hash = Hash[@name_list.zip @tweet_list]
+
+		@name_tweet_hash.each_pair do |one|
+  			@hash_array << one
+  		end
 		
-		#@display_tweet = @name_tweet_hash.rand_pair!
-
-		end
-
+		#@hash_array.shuffle.pop[1]
 
 	end
 	
