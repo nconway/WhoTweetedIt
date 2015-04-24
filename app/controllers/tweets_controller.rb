24e5a2cd90dd33cf9209b0eb8d434b69f51dc535
nconway <nicole.conway@gmail.com>
2015-04-23 18:49:11 -0700

    need to get random working and div rotation

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,12 +4,37 @@ class TweetsController < ApplicationController
   	end
 
 	def show
+		
+		def rand_pair!
+      		k,v = rand_pair
+      		delete( k )
+      		return k,v
+    	end
+
 		client = current_user.twitter_client
 		@friends = client.friends
 		#@friends_array = client.friends.name.to_a
 
 		#binding.pry
 		#puts "aaaaaaaaaaaaaaaaaaaaaaaaa"
+
+		@name_list = Array.new
+		@tweet_list = Array.new
+
+		@friends.attrs[:users].each do |todo|
+  			@name_list.push(todo[:name])
+  			@tweet_list.push(todo[:status][:text])
+
+  		#creates a hash from the two arrays
+		@name_tweet_hash = Hash[@name_list.zip @tweet_list]
+		
+		#@display_tweet = @name_tweet_hash.rand_pair!
+
+		end
+
+
 	end
+	
+	
 
 end
